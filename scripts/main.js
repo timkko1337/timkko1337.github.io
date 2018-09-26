0079b3d2a7c0cf27c89dd405210a9f7aca75ffee
timkko1337 <timkko1337@gmail.com>
2018-09-26 19:26:26 +0300

    test1

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+///Мои переменные:
+var myHeading = document.querySelector('h1')
+var myVar = 'Bob';
+var myNum = 20;
+var myImg = document.querySelector('img');
+///скрипт "Привет мир!"
+myHeading.textContent = 'UNDERTALE. Статья';
+console.log(myVar, myNum, myImg)
